a1bad0fbe871a715766809b3bba581ca3b322fc1
Guilherme Vitória <guivitoria2010@hotmail.com>
2021-07-04 18:03:30 -0300

    Acertando em partes a responsividade do site

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -6,8 +6,14 @@ $('.owl-carousel').owlCarousel({
        0:{
            items:1
        },
+       300:{
+           items:2
+       },
        600:{
            items:3
+       },
+       800:{
+           items:4
        },
        1000:{
            items:5
